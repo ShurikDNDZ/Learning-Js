3979ad52006f31682f39eb505ae123357e229964
ShurikDNDZ <alexanderchupilko@gmail.com>
2016-09-13 01:04:16 +0300

    Add files via upload

---
+++ HomeTask1.js
@@ -0,0 +1,41 @@
+Home Task #1
+
+These tasks will help you to practice skills of working with Variables & Data Types, Conditions and Functions. You can use this Lesson draft as helper: lesson draft. Do tasks in 2 styles: ES5 (current JS version - var) and ES6 (new JS version - let / const).
+
+Your scripts for home task should be published as Gist on Github and sent to me.
+
+Variables
+
+Create next list of variables:
+
+name -- your name as String
+age -- your age as Number
+hasDriverLicense -- do you have driver license as Bool
+grades -- list of your grades for last 5 exams as Array
+person -- Object of your personality with all variables listed above
+After, print name, age and grades with console.log. Also, print your 2nd grade from grades array and person's Object age property.
+
+Conditions
+
+You need to create 2 number variables with random values. Then check, if first variable is bigger (tip: num1 > num2), print 'Number one is bigger', otherwise print 'Number two is bigger';
+
+Functions
+
+Create function which calculates sum of 3 arguments passed to it.
+
+//ES 5:
+
+var name = 'Alexander';
+var age = 30
+var hasDriverLicense = true;
+var grades = [3, 5, 4, 2, 4];
+var person = {
+  name : 'Alexander',
+  age : 30,
+  hasDriverLicense : true,
+  grades : [3, 5, 4, 2, 4]
+};
+    
+document.write(grades[1] + '<br>');
+document.write(person.name);
+After, print name, age and grades with console.log. Also, print your 2nd grade from grades array and person's Object age property.
